861024a61ff55c09c5946f6bb5c0bd2cecf690ce
jk992 <jk-silva@hotmail.com>
2014-08-06 09:51:43 -0300

    testes e scripts

---
+++ main/compilador.bat
@@ -0,0 +1,7 @@
+echo off
+echo Iniciando o Compilador
+echo -----------------------------------------
+java -jar .\compilador.jar %1
+echo -----------------------------------------
+ECHO Compilador finalizado!
+pause
